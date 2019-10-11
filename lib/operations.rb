af55e5e6c1fd95779e28cb427638372cdc4fcd18
Derric Fragosa <fderric@gmail.com>
2019-10-11 04:03:47 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,11 +12,9 @@ end
 def not_safe?(speed)
   speed > 60 || speed < 40 ?  true : false
   
-  #speed < 40 ? true : false
+  
 	#return true speed > 60
 	#return true speed < 40
 	
 end
-	#test-expression ? if-true-expression : if-false-expression
-#(age > 10 ? (score < 80 ? 'behind' : 'above average') : (score < 50 ? 'behind' : 'above average'));
-
+	
