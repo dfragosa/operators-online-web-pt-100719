398958b456775f8b4563787512d684b3d1ac32ee
Derric Fragosa <fderric@gmail.com>
2019-10-11 03:55:57 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,6 +11,7 @@ end
 
 def not_safe?(speed)
   speed > 60 && speed < 40 ?  true : false
+  speed > 60 && speed < 40 ? false : true
   #speed < 40 ? true : false
 	#return true speed > 60
 	#return true speed < 40
